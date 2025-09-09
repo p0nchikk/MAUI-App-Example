790663ad41c227abb93bde07c4a271e740e492b0
tihonist <tihonist@DESKTOP-SHDFRTY>
2025-09-09 14:00:28 +0300

    Playground page was added

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ExampleApp
+﻿using ExampleApp.Views;
+
+namespace ExampleApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Playground();
         }
     }
 }
+
---
+++ Views/Playground.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExampleApp.Views;
+
+public partial class Playground : ContentPage
+{
+	public Playground()
+	{
+		InitializeComponent();
+	}
+}
